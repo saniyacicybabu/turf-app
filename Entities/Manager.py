fe57006f7584978102e4ba84b2ee6ab659da370d
saniyacicybabu <saniyacicybabu@gmail.com>
2023-01-23 22:44:07 +0530

    entities- manage

--- Entities/Manager.py
+++ Entities/Manager.py
@@ -1,22 +1,53 @@
-def Login():
-    pass
+from Entities.User import User
+from Database.Database import Database
+from Entities.Turf import Turf
 
 
-def CheckRates():
-    pass
+class Manager(User):
 
+    def checkRates(self):
+        for turf in Turf.printTurfs():
+            if(turf.managerId == self.id):
+                print(turf)
 
-def ViewRequest():
-    pass
+    def viewRequest(self):
+        pass
 
+    def confirmBooking(self):
+        pass
 
-def ConfirmBooking():
-    pass
+    def generateBill(self):
+        pass
 
+    def bookingsHistory(self):
+        pass
 
-def BillGeneration():
-    pass
+    @staticmethod
+    def printManagers():
+        managerList = Database().getAllManagers()
+        managers = []
+        for manager in managerList:
+            managers.append(Manager(manager[0], manager[1], None))
+        return managers
 
-
-def BookingsHistory():
-    pass
+    def printMenu(self):
+        option = 1
+        while option != 6:
+            print("\nMENU")
+            print("1. Check rates")
+            print("2. View requests")
+            print("3. Confirm booking")
+            print("4. Generate Bill")
+            print("5. View booking history")
+            print("6. Exit")
+            option = int(input("Enter your choice: "))
+            if(option == 1):
+                self.checkRates()
+            elif(option == 2):
+                self.viewRequest()
+            elif(option == 3):
+                self.confirmBooking()
+            elif(option == 4):
+                self.generateBill()
+            elif(option == 5):
+                self.bookingsHistory()
